6a7e870e0b0b6a0e56f68217206bf0ecdd247bc7
itsarunkumarx <arunkumarpalani428@gmail.com>
2026-08-17 23:02:53 +0530

    Enable DEBUG to view exact error traceback

--- EcommerceProject/settings.py
+++ EcommerceProject/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = True
+
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
